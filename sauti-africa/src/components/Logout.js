a9408df7d206f3db9537629e0e08f794adbbb3dd
jacobjolibois <jacobjolibois@hey.com>
2022-01-31 13:16:33 -0600

    commented out code that Vercel would not like

--- sauti-africa/src/components/Logout.js
+++ sauti-africa/src/components/Logout.js
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react';
 // import { useHistory } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 
 const Logout = () => {
 	
-	const { push } = useHistory();
+	// const { push } = useHistory();
 
 	useEffect(() => {
-		axios.post('')
-			.then(res => {
-				console.log(res);
-				localStorage.removeItem('token');
-				push('/login');
-			})
-			.catch(err => console.error(err));
+		// axios.post('')
+		// 	.then(res => {
+		// 		console.log(res);
+		// 		localStorage.removeItem('token');
+		// 		push('/login');
+		// 	})
+		// 	.catch(err => console.error(err));
 	}, []);
 
 	return(<div></div>);
